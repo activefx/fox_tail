46632eba9ca72bbadb37d5b74a1ead959c05656b
James Fawks <jefawks3@gmail.com>
2024-02-01 12:18:25 -0500

    Removing unneeded tests

--- test/fox_tail/config_test.rb
+++ test/fox_tail/config_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class FoxTail::ConfigTest < ActiveSupport::TestCase
-  def test_default_classname_merger
-    assert_kind_of FoxTail::ClassnameMerger, FoxTail::Config.defaults.classname_merger
-  end
-
   def test_default_stimulus_merger
     assert_kind_of FoxTail::StimulusMerger, FoxTail::Config.defaults.stimulus_merger
   end
